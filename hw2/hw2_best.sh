19c1594fd4ec6488e4e52490e9a9f6d5ed134694
zander363 <b04505021@ntu.edu.tw>
2018-04-05 19:13:22 +0800

    for report

---
+++ hw2/hw2_best.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# hw2_logistic.sh
+# Copyright (C) 2018  <@DESKTOP-TA60DPH>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+python logistic.py --infer --test_data_path $5 --output $6
